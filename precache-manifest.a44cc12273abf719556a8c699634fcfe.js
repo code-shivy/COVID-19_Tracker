b252b26fd05b1758a762a30b6b3a19f1be4ad682
Shubham <golusstyle@gmail.com>
2020-07-04 17:29:29 +0530

    Updates

---
+++ precache-manifest.a44cc12273abf719556a8c699634fcfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb78233959917321700b7aaa52b61ac6",
+    "url": "/COVID-19_Tracker/index.html"
+  },
+  {
+    "revision": "093c4d619be4feab9e5b",
+    "url": "/COVID-19_Tracker/static/css/main.aeace3c0.chunk.css"
+  },
+  {
+    "revision": "2c4bb2c65186ac674027",
+    "url": "/COVID-19_Tracker/static/js/2.cd2e1ffb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19_Tracker/static/js/2.cd2e1ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093c4d619be4feab9e5b",
+    "url": "/COVID-19_Tracker/static/js/main.10838cfc.chunk.js"
+  },
+  {
+    "revision": "2037218a15313f0b3e78",
+    "url": "/COVID-19_Tracker/static/js/runtime-main.89d7f55f.js"
+  }
+]);
